714b9311d85c9631319466c0454a87f787962e23
sayimlasa <136328228+sayimlasa@users.noreply.github.com>
2024-01-29 14:05:37 +0300

    commit first

---
+++ app/Models/Role.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    public function run()
+    {
+        $adminRole = Role::create(['name' => 'admin']);
+        $userRole = Role::create(['name' => 'user']);
+
+        $adminRole->givePermissionTo('all');
+    }
+}
